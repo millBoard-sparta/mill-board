50288ae325c9fb9e38c6b0fcc7e14d21ece69360
Rohtable <rohtable@gmail.com>
2024-07-10 12:43:16 +0900

    init

---
+++ src/main/java/com/sparta/millboard/MillboardApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.millboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MillboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MillboardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/millboard/MillboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.millboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MillboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
