94bdd1c50e0b2fe0a835245253c9bf43531f1f8b
rohmac <rohtable@gmail.com>
2024-07-15 21:55:21 +0900

    Modified CommentController to include cardId in delete response

--- src/main/java/com/sparta/millboard/controller/CommentController.java
+++ src/main/java/com/sparta/millboard/controller/CommentController.java
@@ -1,10 +1,12 @@
 package com.sparta.millboard.controller;
 
+import com.sparta.millboard.common.CommonResponse;
 import com.sparta.millboard.dto.request.CommentRequestDto;
 import com.sparta.millboard.dto.response.CommentResponseDto;
 import com.sparta.millboard.service.CommentService;
 import com.sparta.millboard.security.UserPrincipal;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -12,36 +14,60 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/card")
 @RequiredArgsConstructor
 public class CommentController {
 
     private final CommentService commentService;
 
-    @PostMapping("/{cardId}/comment")
-    public ResponseEntity<CommentResponseDto> createComment(@RequestBody CommentRequestDto commentRequestDto,
-                                                            @PathVariable Long cardId,
-                                                            @AuthenticationPrincipal UserPrincipal userPrincipal) {
+    @PostMapping("/api/card/{cardId}/comment")
+    public ResponseEntity<?> createComment(@RequestBody CommentRequestDto commentRequestDto,
+                                           @PathVariable Long cardId,
+                                           @AuthenticationPrincipal UserPrincipal userPrincipal) {
         CommentResponseDto comment = commentService.createComment(commentRequestDto, cardId, userPrincipal);
-        return ResponseEntity.ok(comment);
+        return ResponseEntity.status(HttpStatus.CREATED).body(
+                new CommonResponse<>(
+                        "댓글 생성 성공",
+                        HttpStatus.CREATED.value(),
+                        comment
+                )
+        );
     }
 
-    @GetMapping("/{cardId}/comment")
-    public ResponseEntity<List<CommentResponseDto>> getCommentsByCardId(@PathVariable Long cardId) {
+    @GetMapping("/api/card/{cardId}/comment")
+    public ResponseEntity<?> getCommentsByCardId(@PathVariable Long cardId) {
         List<CommentResponseDto> comments = commentService.getCommentsByCardId(cardId);
-        return ResponseEntity.ok(comments);
+        return ResponseEntity.ok(
+                new CommonResponse<>(
+                        "댓글 전체 조회 성공",
+                        HttpStatus.OK.value(),
+                        comments
+                )
+        );
     }
 
-    @GetMapping("/comment/{commentId}")
-    public ResponseEntity<CommentResponseDto> getComment(@PathVariable Long commentId) {
+    @GetMapping("/api/card/comment/{commentId}")
+    public ResponseEntity<?> getComment(@PathVariable Long commentId) {
         CommentResponseDto comment = commentService.getComment(commentId);
-        return ResponseEntity.ok(comment);
+        return ResponseEntity.ok(
+                new CommonResponse<>(
+                        "댓글 조회 성공",
+                        HttpStatus.OK.value(),
+                        comment
+                )
+        );
     }
 
-    @DeleteMapping("/comment/{commentId}")
-    public ResponseEntity<String> deleteComment(@PathVariable Long commentId,
-                                                @AuthenticationPrincipal UserPrincipal userPrincipal) {
+    @DeleteMapping("/api/card/{cardId}/comment/{commentId}")
+    public ResponseEntity<?> deleteComment(@PathVariable Long cardId,
+                                           @PathVariable Long commentId,
+                                           @AuthenticationPrincipal UserPrincipal userPrincipal) {
         commentService.deleteComment(commentId, userPrincipal);
-        return ResponseEntity.ok("제거 성공");
+        return ResponseEntity.ok(
+                new CommonResponse<>(
+                        "댓글 삭제 성공",
+                        HttpStatus.NO_CONTENT.value(),
+                        null
+                )
+        );
     }
 }
