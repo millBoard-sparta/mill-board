4d97fcb1658e3c6172b899e1f862475d83885e65
namsanghyeok <artexi1464.2@gmail.com>
2024-07-15 21:33:46 +0900

    refactor: 사용하는 생성자에만 builder 사용

--- src/main/java/com/sparta/millboard/model/Board.java
+++ src/main/java/com/sparta/millboard/model/Board.java
@@ -3,33 +3,34 @@ package com.sparta.millboard.model;
 import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.experimental.SuperBuilder;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 import java.util.Set;
 
 @Entity
 @Getter
-@RequiredArgsConstructor
+@NoArgsConstructor
 @Table(name = "boards")
-@SuperBuilder
 public class Board {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "board_id")
     private Long id;
-
     private String title;
-
     private String description;
-
     @OneToMany(mappedBy = "board", cascade = CascadeType.ALL, orphanRemoval = true)
     private Set<BoardColumn> boardColumns;
-
     @OneToMany(mappedBy = "board", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<BoardPartner> boardPartners;
+
+    @Builder
+    public Board(String title, String description, Set<BoardColumn> boardColumns, List<BoardPartner> boardPartners) {
+        this.title = title;
+        this.description = description;
+        this.boardColumns = boardColumns;
+        this.boardPartners = boardPartners;
+    }
 
     public void update(Board entity) {
         if (entity.getTitle() != null && !entity.getTitle().isEmpty()) {
